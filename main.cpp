12a3578d78d39e2d6142ec26bb0da1abef4c4586
Megan <mko5258@psu.edu>
2023-10-05 01:54:00 -0400

    Completed code with functions

--- main.cpp
+++ main.cpp
@@ -169,3 +169,48 @@ int main()
 
 	return 0;
 }
+
+vector<double> gradient_weights(vector <double>& w, vector <double>& x, double y)
+{
+
+	double result = 0; //start the result at zero, then values will be added 
+	//to it zero the for loop.
+	for (int i = 0; i < w.size(); i++) //both vectors will be same size, so run
+		//loop for number of elements within vectors
+	{
+		result += w[i] * x[i]; //new result is equal to the original result 
+		//plus the product of w*x (dot product is multiplying respective terms
+		//and adding the products of the terms together
+	}
+
+	z = result;
+
+	sigma = 1 / (1 + exp(-z)); //function given in doc
+	sigmaprime = sigma * (1 - sigma); //function given in doc
+
+	y_predict = sigma;
+
+	dC = 2 * (y_predict - y); //function given in doc
+
+	vector<double> dw = { 1, 1, 1 };
+
+	for (int i = 0; i < dw.size(); i++)
+	{
+		dw[i] = dC * sigmaprime * x[i]; // multiplies every element of vector x and creates
+		//new vector dw for gradient weights
+	}
+	return dw;
+}
+
+vector<double> update_weights(vector <double>& dw, vector <double>& w, double alpha)
+{
+
+	for (int i = 0; i < w.size(); i++)
+	{
+		w[i] = w[i] - alpha * dw[i]; //each element of dw is multiplied by alpha
+		//then subtracted from the respective element in w to create new vector of updated
+		//w values
+	}
+	return w;
+
+}
